34b74b689268396912b6237405e8533194d9355c
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-03 14:52:48 +0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Takumi Otsuka on 3/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
