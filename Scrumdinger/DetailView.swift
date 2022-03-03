bf03c017c112f716a04eea0d8b3a898cbdc9bc66
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-03 18:00:58 +0800

    Initial Commit

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Takumi Otsuka on 3/3/22.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
