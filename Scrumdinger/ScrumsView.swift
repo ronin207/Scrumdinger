64b1d9264e0d12f771ee744139f73451a6050eef
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-03 17:12:31 +0800

    Initial Commit

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,20 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Takumi Otsuka on 3/3/22.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView()
+    }
+}
