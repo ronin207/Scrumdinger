c1a0a54bf959e86a389113669d4b9bb8e6d9507d
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-03 14:58:36 +0800

    Renamed ContentView to MeetingView

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MeetingView.swift
 //  Scrumdinger
 //
 //  Created by Takumi Otsuka on 3/3/22.
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MeetingView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MeetingView()
     }
 }
