c15bbc484b8f38c20d6e90a4cfa8f6ee0dbf3e75
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-03 21:24:25 +0800

    Added TextField to this file where we can add newAttendees name to the Scrum Data

--- DetailEditView.swift
+++ DetailEditView.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct DetailEditView: View {
+    @State private var data = DailyScrum.Data()
+    @State private var newAttendeeName = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section {
+                TextField("Title", text: $data.title)
+                
+                HStack {
+                    Slider(value: $data.lengthInMinutes, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                    
+                    Spacer()
+                    
+                    Text("\(Int(data.lengthInMinutes)) minute(s)")
+                }
+            } header: {
+                Text("Meeting Info")
+            }
+            
+            Section {
+                ForEach(data.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                    data.attendees.remove(atOffsets: indices)
+                }
+                
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(action: {
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            data.attendees.append(attendee)
+                            newAttendeeName = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
+                }
+            } header: {
+                Text("Attendees")
+            }
+        }
     }
 }
 
