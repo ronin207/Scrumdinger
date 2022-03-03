7a9f7d9c9316909c3781fbad4ce34d0297d2b8ef
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-03 17:18:35 +0800

    Updated DailyScrum model with id and created an initializer of the variables in the model structure.

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -7,11 +7,20 @@
 
 import SwiftUI
 
-struct DailyScrum {
+struct DailyScrum: Identifiable {
+    let id: UUID
     var title: String
     var attendees: Array<String>
     var lengthInMinutes: Int
     var theme: Theme
+    
+    init(id: UUID = UUID(), title: String, attendees: Array<String>, lengthInMinutes: Int, theme: Theme) {
+        self.id = id
+        self.title = title
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+        self.theme = theme
+    }
 }
 
 extension DailyScrum {
