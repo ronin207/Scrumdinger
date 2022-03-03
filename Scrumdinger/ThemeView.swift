b0021abb8597d66b942399590fc73acc91df6bcf
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-03 22:02:05 +0800

    Created a ThemeView where the highlight and label of the theme is presented.

--- Scrumdinger/ThemeView.swift
+++ Scrumdinger/ThemeView.swift
@@ -8,13 +8,23 @@
 import SwiftUI
 
 struct ThemeView: View {
+    let theme: Theme
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RoundedRectangle(cornerRadius: 4)
+                .fill(theme.mainColor)
+            
+            Label(theme.name, systemImage: "paintpalette")
+                .padding(4)
+        }
+        .foregroundColor(theme.accentColor)
+        .fixedSize(horizontal: false, vertical: true)
     }
 }
 
 struct ThemeView_Previews: PreviewProvider {
     static var previews: some View {
-        ThemeView()
+        ThemeView(theme: .buttercup)
     }
 }
