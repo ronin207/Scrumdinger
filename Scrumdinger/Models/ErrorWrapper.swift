3cd00fbef3b5404a76d6bc7cca4ab726b3fc4842
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-04 15:40:35 +0800

    Initial Commit

---
+++ Scrumdinger/Models/ErrorWrapper.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorWrapper.swift
+//  Scrumdinger
+//
+//  Created by Takumi Otsuka on 4/3/22.
+//
+
+import SwiftUI
+
+struct ErrorWrapper: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ErrorWrapper_Previews: PreviewProvider {
+    static var previews: some View {
+        ErrorWrapper()
+    }
+}
