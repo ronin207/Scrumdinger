5763e2ab721f81d45e33a45610a0265b027fadd0
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-04 12:58:14 +0800

    Initial Commit

---
+++ Scrumdinger/Models/AVPlayer+Ding.swift
@@ -0,0 +1,20 @@
+//
+//  AVPlayer+Ding.swift
+//  Scrumdinger
+//
+//  Created by Takumi Otsuka on 4/3/22.
+//
+
+import SwiftUI
+
+struct AVPlayer_Ding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AVPlayer_Ding_Previews: PreviewProvider {
+    static var previews: some View {
+        AVPlayer_Ding()
+    }
+}
