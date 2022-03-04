48308cb8f9e36a7d5a816ff321fb5475fa5f3dba
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-04 14:41:57 +0800

    Initial Commit

---
+++ Scrumdinger/Models/History.swift
@@ -0,0 +1,20 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by Takumi Otsuka on 4/3/22.
+//
+
+import SwiftUI
+
+struct History: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct History_Previews: PreviewProvider {
+    static var previews: some View {
+        History()
+    }
+}
