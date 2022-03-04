cfa6f1a6bfa7b0454a0f7087609fa5c784627cb9
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-04 16:24:12 +0800

    Added an overlay over the circle to display the arc which changes over the number of speakers spoken.

--- Scrumdinger/MeetingTimerView.swift
+++ Scrumdinger/MeetingTimerView.swift
@@ -28,6 +28,15 @@ struct MeetingTimerView: View {
                 .accessibilityElement(children: .combine)
                 .foregroundColor(theme.accentColor)
             }
+            .overlay {
+                ForEach(speakers) { speaker in
+                    if speaker.isCompleted, let index = speakers.firstIndex(where: { $0.id == speaker.id }) {
+                        SpeakerArc(speakerIndex: index, totalSpeakers: speakers.count)
+                            .rotation(Angle(degrees: -90))
+                            .stroke(theme.mainColor, lineWidth: 12)
+                    }
+                }
+            }
     }
 }
 
