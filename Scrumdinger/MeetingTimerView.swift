b29640bda385db69bfdaed9b4fdfa93dd897016f
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-04 16:03:22 +0800

    Initial Commit

---
+++ Scrumdinger/MeetingTimerView.swift
@@ -0,0 +1,20 @@
+//
+//  MeetingTimerView.swift
+//  Scrumdinger
+//
+//  Created by Takumi Otsuka on 4/3/22.
+//
+
+import SwiftUI
+
+struct MeetingTimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeetingTimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingTimerView()
+    }
+}
