098d6685f05e32b55f6f4c4849faafc6d5be9b9e
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-04 15:00:40 +0800

    Initial Commit

---
+++ Scrumdinger/Models/ScrumStore.swift
@@ -0,0 +1,20 @@
+//
+//  ScrumStore.swift
+//  Scrumdinger
+//
+//  Created by Takumi Otsuka on 4/3/22.
+//
+
+import SwiftUI
+
+struct ScrumStore: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScrumStore_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumStore()
+    }
+}
