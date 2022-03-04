27e00bee0b21db79e6195c61cfb323adac48bb09
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-04 15:18:27 +0800

    Allows the view state and action to change based on the scene phase.

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -8,9 +8,14 @@
 import SwiftUI
 
 struct ScrumsView: View {
+    @Environment(\.scenePhase) private var scenePhase
+    
     @State private var isPresentingNewScrumView = false
     @State private var newScrumData = DailyScrum.Data()
+    
     @Binding var scrums: Array<DailyScrum>
+    
+    let saveAction: () -> Void
     
     var body: some View {
         List {
@@ -52,13 +57,16 @@ struct ScrumsView: View {
                     }
             }
         }
+        .onChange(of: scenePhase) { phase in
+            if phase == .inactive { saveAction() }
+        }
     }
 }
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            ScrumsView(scrums: .constant(DailyScrum.sampleData))
+            ScrumsView(scrums: .constant(DailyScrum.sampleData), saveAction: {})
         }
     }
 }
